afeafd43bdba505382b7f3b41cb9cfc6a697b167
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-08-20 22:41:53 +0900

    :sparkles:Setting color number (harib01f)

--- bootpack.c
+++ bootpack.c
@@ -1,18 +1,73 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+
 
 void HariMain(void)
 {
     int i;
+    char *p;  /* BYTE */
 
-    for(i=0xa0000; i<0xaffff; ++i) {
-        //write_mem8(i, i & 0x0f);
-        //write_mem8(i, 15);
-        write_mem8(i, i % 256);
+    init_palette();
+
+    p = (char *) 0xa0000;
+
+    for(i=0; i<=0xffff; ++i) {
+        p[i] = i & 0x0f;
     }
 
     for(;;) {
         io_hlt();
     }
 
+}
+
+void init_palette(void)
+{
+    static unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00,  /*  0: Black        */
+        0xff, 0x00, 0x00,  /*  1: Light Red    */
+        0x00, 0xff, 0x00,  /*  2: Light Green  */
+        0xff, 0xff, 0x00,  /*  3: Light Yellow */
+        0x00, 0x00, 0xff,  /*  4: Light Blue   */
+        0xff, 0x00, 0xff,  /*  5: Light Purple */
+        0x00, 0xff, 0xff,  /*  6: Light Aqua   */
+        0xff, 0xff, 0xff,  /*  7: White        */
+        0xc6, 0xc6, 0xc6,  /*  8: Light Gray   */
+        0x84, 0x00, 0x00,  /*  9: Dark Red     */
+        0x00, 0x84, 0x00,  /* 10: Dark Green   */
+        0x84, 0x84, 0x00,  /* 11: Dark Yellow  */
+        0x00, 0x00, 0x84,  /* 12: Dark Blue    */
+        0x84, 0x00, 0x84,  /* 13: Dark Purple  */
+        0x00, 0x84, 0x84,  /* 14: Dark Aqua    */
+        0x84, 0x84, 0x84   /* 15: Dark Gray    */
+    };
+
+    set_palette(0, 15, table_rgb);
+
+    return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+    int i, eflags;
+
+    eflags = io_load_eflags();
+    io_cli();
+    io_out8(0x03c8, start);
+    for(i=start; i<=end; ++i) {
+        io_out8(0x03c9, rgb[0] / 4);
+        io_out8(0x03c9, rgb[1] / 4);
+        io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    io_store_eflags(eflags);
+
+    return;
 }

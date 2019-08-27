3592652bf619fefd3d996d893a0d74387dad7c14
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-08-27 14:55:24 +0900

    :bookmark:[Day10] (harib07d)

--- bootpack.c
+++ bootpack.c
@@ -61,7 +61,7 @@ void HariMain(void)
             memtotal / (1024 * 1024), memman_total(memman) / 1024);
     putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 
-    sheet_refresh(shtctl);
+    sheet_refresh(shtctl, sht_back, 0, 0, binfo->scrnx, 48);
 
     for(;;) {
         io_cli();
@@ -74,7 +74,7 @@ void HariMain(void)
                 sprintf_(s, "%02X", i);
                 boxfill8(buf_back, binfo->scrnx, COL8_008484,  0, 16, 15, 31);
                 putfonts8_asc(buf_back, binfo->scrnx,  0, 16, COL8_FFFFFF, s);
-                sheet_refresh(shtctl);
+                sheet_refresh(shtctl, sht_back, 0, 16, 16, 32);
             } else if(fifo8_status(&mousefifo) != 0) {
                 i = fifo8_get(&mousefifo);
                 io_sti();
@@ -91,6 +91,7 @@ void HariMain(void)
                     }
                     boxfill8(buf_back, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
                     putfonts8_asc(buf_back, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                    sheet_refresh(shtctl, sht_back, 32, 16, 32 + 15 * 8, 32);
 
                     /* Move mouse cursor */
                     boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15); /* Delete cursor */
@@ -111,6 +112,7 @@ void HariMain(void)
                     sprintf_(s, "(%3d, %3d)", mx, my);
                     boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
                     putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+                    sheet_refresh(shtctl, sht_back, 0, 0, 80, 16);
                     sheet_slide(shtctl, sht_mouse, mx, my);
                 }
             }

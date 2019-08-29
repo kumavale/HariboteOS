47e25e0c032c8c9aadd37ed85f09119e789e6b31
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-08-29 16:04:13 +0900

    :sparkles:[Day12] (harib09g)

--- bootpack.h
+++ bootpack.h
@@ -175,14 +175,22 @@ void sheet_slide(struct SHEET *sht, int vx0, int vy0);
 void sheet_free(struct SHEET *sht);
 
 /* timer.c */
-struct TIMERCTL {
-    unsigned int count;
-    unsigned int timeout;
+#define MAX_TIMER 500
+struct TIMER {
+    unsigned int timeout, flags;
     struct FIFO8 *fifo;
     unsigned char data;
 };
+struct TIMERCTL {
+    unsigned int count, next, using;
+    struct TIMER *timers[MAX_TIMER];
+    struct TIMER timers0[MAX_TIMER];
+};
 extern struct TIMERCTL timerctl;
 void init_pit(void);
+struct TIMER *timer_alloc(void);
+void timer_free(struct TIMER *timer);
+void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data);
+void timer_settime(struct TIMER *timer, unsigned int timeout);
 void inthanler20(int *esp);
-void settimer(unsigned int timeout, struct FIFO8 *fifo, unsigned char data);
 

ac16759ff9c43a58e4822d3b77eead9a3df50873
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-08-31 09:08:33 +0900

    :sparkles:Adding input key (harib11g)

--- bootpack.c
+++ bootpack.c
@@ -17,8 +17,16 @@ void HariMain(void)
     char s[64];
     unsigned char *buf_back, buf_mouse[256], *buf_win;
     int fifobuf[128];
-    int mx, my, i, count = 0;
+    int mx, my, i;
     unsigned int memtotal;
+    static char keytable[0x54] = {
+         0 ,  0 , '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^',  0 ,  0 ,
+        'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[',  0 ,  0 , 'A', 'S',
+        'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':',  0 ,  0 , ']', 'Z', 'X', 'C', 'V',
+        'B', 'N', 'M', ',', '.', '/',  0 , '*',  0 , ' ',  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
+         0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 , '7', '8', '9', '-', '4', '5', '6', '+', '1',
+        '2', '3', '0', '.'
+    };
 
     init_gdtidt();
     init_pic();
@@ -63,7 +71,7 @@ void HariMain(void)
     init_screen(buf_back, binfo->scrnx, binfo->scrny);
     init_mouse_cursor8(buf_mouse, 99);
 
-    make_window8(buf_win, 160, 52, "counter");
+    make_window8(buf_win, 160, 52, "window");
 
     sheet_slide(sht_back, 0, 0);
     mx = (binfo->scrnx - 16) / 2;
@@ -82,18 +90,24 @@ void HariMain(void)
     putfonts8_asc_sht(sht_back, 0, 32, COL8_FFFFFF, COL8_008484, s, 40);
 
     for(;;) {
-        ++count;
-
         io_cli();
         if (fifo32_status(&fifo) == 0) {
-            //io_stihlt();
-            io_sti();
+            io_stihlt();
+            //io_sti();
         } else {
             i = fifo32_get(&fifo);
             io_sti();
             if (256 <= i && i <= 511) {  /* Keyboard */
                 sprintf_(s, "%02X", i - 256);
                 putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
+
+                if (i < 256 + 0x54) {
+                    if (keytable[i - 256] != 0) {
+                        s[0] = keytable[i - 256];
+                        s[1] = 0;
+                        putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 1);
+                    }
+                }
             } else if (512 <= i && i <= 767) {  /* Mouse */
                 if (mouse_decode(&mdec, i - 512) != 0) {
                     sprintf_(s, "[lcr %3d %3d]", mdec.x, mdec.y);
@@ -129,11 +143,8 @@ void HariMain(void)
                 }
             } else if (i == 10) {  /* 10sec timer */
                 putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
-                sprintf_(s, "%010d", count);
-                putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 10);
             } else if (i == 3) {  /* 3sec timer */
                 putfonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[sec]", 6);
-                count = 0;
             } else if (i == 1) {  /* timer for cursor */
                 timer_init(timer3, &fifo, 0);
                 boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);

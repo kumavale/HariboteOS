6fc1d8c3e471c8695d5ce1443b323f40c2d19d31
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-09-04 21:48:20 +0900

    :art:Refactoring (harib17a)

--- bootpack.c
+++ bootpack.c
@@ -1,15 +1,6 @@
 #include "bootpack.h"
 #include "mystd.h"
 
-void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
-void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
-void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
-void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
-void console_task(struct SHEET *sheet, unsigned int memtotal);
-int cons_newline(int cursor_y, struct SHEET *sheet);
-void file_readfat(int *fat, unsigned char *img);
-void file_loadfile(int clustno, int size, char *buf, int *fat, char *img);
-
 #define KEYCMD_LED 0xed
 
 
